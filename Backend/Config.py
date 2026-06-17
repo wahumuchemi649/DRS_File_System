04afb14765f7c846c3f706fc7d3bf6965d23caf6
Irene Shipapa <nasiekuirene6@gmail.com>
2026-06-17 10:45:49 +0300

    Switch DB config to use .env

--- Backend/Config.py
+++ Backend/Config.py
@@ -1,9 +1,21 @@
 
 from sqlalchemy import create_engine
 from sqlalchemy.orm import declarative_base, sessionmaker
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_HOST = os.getenv("DB_HOST", "localhost")
+DB_PORT = os.getenv("DB_PORT", "3306")
+DB_NAME = os.getenv("DB_NAME", "DRS_File_System")
 
 
-DATABASE_URL = "mysql+pymysql://root:42131607k@localhost:3306/DRS_File_System"
+
+
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # The engine acts as the core interface to the database
 engine = create_engine(DATABASE_URL, echo=False)
---
+++ Backend/Registry/register_document.py
@@ -0,0 +1,42 @@
+from Registry.models import Document
+from Config import SessionLocal
+
+def register_document(document:Document):
+  db = SessionLocal()
+  try:
+    count = db.query(Document).count()
+    ref_no = f"REF-{count + 1:04d}" #generate ref no e.g REF-0001...
+
+    #NEW DOCUMENT OBJECT
+    new_document = Document(
+      refNo=ref_no,
+      docType=document.docType,
+      dateCreated=document.dateCreated,
+      IsExternal=document.IsExternal,
+      title=document.title,
+      filePath=document.filePath,
+      deptId=document.deptId,
+      status="Registered"
+    )
+
+    db.add(new_document)
+    db.commit()
+    db.refresh(new_document)
+
+    return {"message": "Document registered successfully", "ref_no": new_document.refNo}
+
+  except Exception as e:
+      return {"error": str(e)}
+
+if __name__ == "__main__":
+    test_doc = Document(
+        docId=999,
+        docType="Report",
+        IsExternal=False,
+        title="Budget Report",
+        filePath="/files/budget.pdf",
+        deptId=None
+    )
+
+    result = register_document(test_doc)
+    print(result)
